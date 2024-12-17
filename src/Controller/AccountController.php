e7bae05ca9f65140c79169db96dfebb296f1f72f
Mathieu Le Puil <lepuilmathieu@gmail.com>
2024-12-17 15:20:48 +0100

    feat: create AccountType to create new form

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -3,8 +3,10 @@
 namespace App\Controller;
 
 use App\Entity\Account;
+use App\Form\AccountType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -28,6 +30,33 @@ class AccountController extends AbstractController
         return $this->render('account/index.html.twig', [
             'controller_name' => 'AccountController',
             'accounts' => $repository->findAll(),
+        ]);
+    }
+
+    #[Route('/account/new', name: 'app_account_new')]
+    public function new(Request $request): Response
+    {
+        if (!$this->getUser()) {
+            return $this->redirectToRoute('app_login');
+        }
+
+        $account = new Account();
+
+        $form = $this->createForm(AccountType::class, $account);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->entityManager->persist($account);
+            $this->entityManager->flush();
+
+            $this->addFlash('success', 'Le compte a été créé avec succès !');
+
+            return $this->redirectToRoute('app_account');
+        }
+
+        return $this->render('account/new.html.twig', [
+            'controller_name' => 'AccountController',
+            'form' => $form->createView(),
         ]);
     }
 }
