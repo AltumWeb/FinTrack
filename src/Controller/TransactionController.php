5909c82967c0edde12bdb343d279649b246dc6f6
Mathieu Le Puil <lepuilmathieu@gmail.com>
2024-12-19 14:09:44 +0100

    feat: add TransactionType and form for new transaction

--- src/Controller/TransactionController.php
+++ src/Controller/TransactionController.php
@@ -3,8 +3,10 @@
 namespace App\Controller;
 
 use App\Entity\Transaction;
+use App\Form\TransactionType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -28,6 +30,33 @@ class TransactionController extends AbstractController
         return $this->render('transaction/index.html.twig', [
             'controller_name' => 'TransactionController',
             'transactions' => $repository->findAll(),
+        ]);
+    }
+
+    #[Route('/transaction/new', name: 'app_transaction_new')]
+    public function new(Request $request): Response
+    {
+        if (!$this->getUser()) {
+            return $this->redirectToRoute('app_login');
+        }
+
+        $transaction = new Transaction();
+
+        $form = $this->createForm(TransactionType::class, $transaction);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->entityManager->persist($transaction);
+            $this->entityManager->flush();
+
+            $this->addFlash('success', 'La transaction a été ajoutée avec succès !');
+
+            return $this->redirectToRoute('app_transaction');
+        }
+
+        return $this->render('transaction/new.html.twig', [
+            'controller_name' => 'TransactionController',
+            'form' => $form->createView(),
         ]);
     }
 }
